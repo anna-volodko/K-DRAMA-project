b8084f153b798c12db3d9832ba142dea8432cc1d
Anna Volodko <anyavolodko95@gmail.com>
2023-11-09 18:10:39 +0200

    first steps to projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import "./styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav">
+        <p className="logo">K-DRAMATIC</p>
+        <ul className="main_nav">
+         <li className="nav_item">HOME</li>
+         <li className="nav_item">EXPLORE</li>
+         <li className="nav_item">PROFILE</li>
+        </ul>
       </header>
+      <main>
+        <div className="about">
+          <h1 className="site_name">K-DRAMATIC</h1>
+          <p className="goal">
+            Your Daily Dose of Emotions from the Best K-Dramas.
+          </p>
+        </div>
+        <input type="text" className="search_input" />
+        <button className="search_button">Search</button>
+      </main>
     </div>
   );
 }
